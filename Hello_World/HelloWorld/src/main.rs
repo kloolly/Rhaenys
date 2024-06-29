3506063b89d2989f841ec3378106ead6d94f9898
kloolly <80373910+kloolly@users.noreply.github.com>
2024-06-29 10:56:20 +0700

    First Commit

---
+++ Hello_World/HelloWorld/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let name = "world";
+    println!("Hello, {}!", name);
+
+    let one = 1;
+    let two = 22;
+    let three = 333;
+    let four = 4444;
+    println!("{},{},{},{}",one,two,three,four); // ปริ้นตัวแปร
+    println!("{}",four+1);
+    println!("{}",four);
+    println!("{}",four);
+
+    // three+=1;
+    let mut five=55555;
+    five+=1;
+    print!("{}",five);
+
+
+}
